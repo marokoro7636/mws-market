ef6a7ed2d3f2eb0a558c1e6054c4536512270d8a
NanbuHarumasa <nanbu.kenkyu@gmail.com>
2023-09-18 02:07:40 +0900

    implement add,get,delete required_spec func

--- api/models/projects.py
+++ api/models/projects.py
@@ -1,7 +1,7 @@
 from firebase_admin import firestore
 from helper.util import sha1_hash
 
-from models.requests import ProjectInfo, ProjectRequest
+from models.requests import ProjectInfo, ProjectRequest, RequiredSpec, SimpleSpecResponse
 
 
 class Project:
@@ -99,18 +99,33 @@ class Project:
     # required_specに関して記述していく
     ## add function
     @staticmethod
-    def add_project_required_spec(project_id, required_spec):
+    def add_project_required_spec(project_id, spec_id, required_spec):
         db = firestore.client()
-        db.collection("projects").document(project_id).set
+        print(type(required_spec))
+        db.collection("projects").document(project_id).collection("ProjectDetails").document(spec_id).set(
+            {
+                "item" : required_spec.item,
+                "required" : required_spec.required
+            }
+        )
 
     ## get function
     @staticmethod
-    def get_project_required_specs(project_id):
-        return
+    def get_project_required_specs(project_id, spec_id):
+        db = firestore.client()
+        docs = db.collection("projects").document(project_id).collection("ProjectDetails").stream()
+        # if not docs.exist:
+        #   return None
+        data = [{"spec_id": doc.id, "data" :doc.to_dict()} for doc in docs ]
+        return data
 
     ## delete function
     @staticmethod
-    def delete_project_required_spec(project_id, required_spec_id):
+    def delete_project_required_spec(project_id, spec_id):
+        db = firestore.client()
+        # if not docs.exist:
+        #   return None
+        db.collection("projects").document(project_id).collection("ProjectDetails").document(spec_id).delete()
         return
 
     # get_projectsの実装 by Yamamoto
