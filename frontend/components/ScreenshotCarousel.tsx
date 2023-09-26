72dbdd9cb4f16326bc5e51cbf7e67d0517c49ad7
marokoro7636 <64199605+marokoro7636@users.noreply.github.com>
2023-09-26 15:51:49 +0900

    Change carousel

--- frontend/components/ScreenshotCarousel.tsx
+++ frontend/components/ScreenshotCarousel.tsx
@@ -1,26 +1,30 @@
 "use client"
 import React from 'react';
-import Carousel from "react-material-ui-carousel";
 import {Box} from "@mui/material";
+import AliceCarousel from "react-alice-carousel";
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 interface ScreenshotCarouselProps {
     imgList: string[]
 }
 
 const ScreenshotCarousel = ({imgList}: ScreenshotCarouselProps) => {
+    const imgWidth = 500 // スクリーンショットの横幅
+    const spacing = 20 // スクリーンショット同士の間
+
     return (
-        <Box sx={{width: "600px"}}>
-            <Carousel
-                autoPlay={false}
-                navButtonsAlwaysVisible={true}
-                animation="slide"
-                cycleNavigation={true}
-            >
-                {
-                    imgList.map((item, i) => <Box component="img" src={item} key={i}/>)
-                }
-            </Carousel>
-        </Box>
+        <AliceCarousel
+            mouseTracking
+            autoWidth
+            infinite
+            items={
+                imgList.map((item, i) => (
+                    <Box width={imgWidth+spacing}>
+                        <Box component="img" src={item} key={i} draggable={false} width={imgWidth}/>
+                    </Box>
+                ))
+            }
+        />
     );
 };
 
