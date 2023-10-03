f23ef42ba601fc1b698f2ed2699d9540e10311cb
marokoro7636 <64199605+marokoro7636@users.noreply.github.com>
2023-10-03 21:15:50 +0900

    Make the design the same as the team screen

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,181 +1,102 @@
 "use client"
-import {Button, Container, Stack, TextField, Typography} from "@mui/material";
-import {MuiFileInput} from "mui-file-input";
-import {Controller, SubmitHandler, useForm} from "react-hook-form";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    Container,
+    Grid,
+    TextField,
+    Typography
+} from "@mui/material";
+import {grey} from "@mui/material/colors";
+import React, {useRef, useState} from "react";
 
-interface Inputs {
+interface AppInfo {
+    id: string
     name: string,
     description: string,
     youtube: string,
-    icon: File,
-    screenshots: File[]
 }
-export default function Register() {
-    const {control, handleSubmit} = useForm<Inputs>({
-        defaultValues: {
-            name: "",
-            description: "",
-            youtube: "",
-            icon: undefined,
-            screenshots: [undefined, undefined, undefined]
-        }
-    })
 
-    const onSubmit: SubmitHandler<Inputs> = async (inputs) => {
-        try {
-            console.log(inputs)
-        } catch (e) {
-            alert(e)
+export default function Register() {
+    const appInfoMock: AppInfo = {
+        id: "0",
+        name: "App 0",
+        description: "description 0 ".repeat(50),
+        youtube: "https://youtu.be/ZaZMZ9jePKw?si=x96UEomj8VWoHw0-"
+    }
+
+    const app_name = useRef<HTMLInputElement>()
+    const app_description = useRef<HTMLInputElement>()
+    const app_youtube = useRef<HTMLInputElement>()
+
+    const [isAppInfoEditable, setAppInfoEditable] = useState<boolean>(false)
+    const [appInfo, setAppInfo] = useState<AppInfo>(appInfoMock)
+
+    const onSaveAppInfo = () => {
+        if (app_name.current?.value != appInfo.name) {
+            console.log("name changed")
+            // TODO: update name by API
+            setAppInfo({...appInfo, name: (app_name.current?.value as string)})
         }
+        if (app_description.current?.value != appInfo.description) {
+            console.log("description changed")
+            setAppInfo({...appInfo, description: (app_description.current?.value as string)})
+        }
+        if (app_youtube.current?.value != appInfo.youtube) {
+            console.log("youtube changed")
+            setAppInfo({...appInfo, youtube: (app_youtube.current?.value as string)})
+        }
+        setAppInfoEditable(false)
     }
 
     return (
         <>
-            <Container sx={{mt: 3}}>
-                <Stack component="form" onSubmit={handleSubmit(onSubmit)} spacing={2}>
-                    <Typography variant="h3">アプリの登録</Typography>
-                    <Controller
-                        control={control}
-                        rules={{
-                            required: "入力してください"
-                        }}
-                        render={({field, fieldState: {error}}) => (
-                                <TextField
-                                    label="アプリ名"
-                                    {...field}
-                                    error={!!error}
-                                    helperText={error?.message as string}
-                                />
-                            )}
-                        name="name"
-                    />
-                    <Controller
-                        control={control}
-                        rules={{
-                            required: "入力してください"
-                        }}
-                        render={({field, fieldState: {error}}) => (
-                                <TextField
-                                    label="アプリの説明"
-                                    multiline
-                                    rows={10}
-                                    {...field}
-                                    error={!!error}
-                                    helperText={error?.message as string}
-                                />
-                            )}
-                        name="description"
-                    />
-                    <Controller
-                        control={control}
-                        rules={{
-                            required: "入力してください"
-                        }}
-                        render={({field, fieldState: {error}}) => (
-                                <TextField
-                                    label="YouTubeリンク"
-                                    {...field}
-                                    error={!!error}
-                                    helperText={error?.message as string}
-                                />
-                            )}
-                        name="youtube"
-                    />
-                    <Typography variant="h5">アプリアイコン</Typography>
-                    <Controller
-                        control={control}
-                        rules={{
-                            required: "入力してください",
-                            validate: (value) => {
-                                if(value.type === "image/png" || value.type === "image/jpeg"){
-                                    return true
-                                } else {
-                                    return "pngもしくはjpg画像を選択してください";
+            <Container sx={{bgcolor: grey[100]}}>
+                <Typography variant="h3" sx={{mt: 3}}>アプリの登録</Typography>
+                <Card sx={{minWidth: 275, mt: 3}}>
+                    <CardHeader title="App Info"/>
+                    <CardContent>
+                        <Grid container spacing={2}>
+                            <Grid item xs={3}>App Name :</Grid>
+                            <Grid item xs={9}>
+                                {isAppInfoEditable ?
+                                    <TextField size="small" variant="standard" inputRef={app_name}
+                                               defaultValue={appInfo.name}/> :
+                                    <Typography variant="body1">{appInfo.name}</Typography>
                                 }
+                            </Grid>
+                            <Grid item xs={3}>Description :</Grid>
+                            <Grid item xs={9}>
+                                {isAppInfoEditable ?
+                                    <TextField fullWidth multiline size="small" variant="standard"
+                                               inputRef={app_description}
+                                               defaultValue={appInfo.description}/> :
+                                    <Typography variant="body1">{appInfo.description}</Typography>
+                                }
+                            </Grid>
+                            <Grid item xs={3}>YouTube Link :</Grid>
+                            <Grid item xs={9}>
+                                {isAppInfoEditable ?
+                                    <TextField size="small" variant="standard" inputRef={app_youtube}
+                                               defaultValue={appInfo.youtube} sx={{width: 500}}/> :
+                                    <Typography component="a" variant="body1"
+                                                href={appInfo.youtube}>{appInfo.youtube}</Typography>
+                                }
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                    <CardActions>
+                        <Grid container justifyContent="flex-end">
+                            {isAppInfoEditable ?
+                                <Button size="small" onClick={onSaveAppInfo}>Save</Button> :
+                                <Button size="small" onClick={() => setAppInfoEditable(true)}>Edit</Button>
                             }
-                        }}
-                        render={({field, fieldState: {error}}) => (
-                                <MuiFileInput
-                                    {...field}
-                                    error={!!error}
-                                    helperText={error?.message as string}
-                                    inputProps={{accept: ".png, .jpeg, .jpg"}}
-                                />
-                            )}
-                        name="icon"
-                    />
-
-                    <Typography variant="h5">スクリーンショット(3枚)</Typography>
-                    <Stack direction="row" spacing={2}>
-                        <Controller
-                            control={control}
-                            rules={{
-                                required: "入力してください",
-                                validate: (value) => {
-                                    if(value.type === "image/png" || value.type === "image/jpeg"){
-                                        return true
-                                    } else {
-                                        return "pngもしくはjpg画像を選択してください";
-                                    }
-                                }
-                            }}
-                            render={({field, fieldState: {error}}) => (
-                                <MuiFileInput
-                                    {...field}
-                                    error={!!error}
-                                    helperText={error?.message as string}
-                                    inputProps={{accept: ".png, .jpeg, .jpg"}}
-                                />
-                            )}
-                            name="screenshots.0"
-                        />
-                        <Controller
-                            control={control}
-                            rules={{
-                                required: "入力してください",
-                                validate: (value) => {
-                                    if(value.type === "image/png" || value.type === "image/jpeg"){
-                                        return true
-                                    } else {
-                                        return "pngもしくはjpg画像を選択してください";
-                                    }
-                                }
-                            }}
-                            render={({field, fieldState: {error}}) => (
-                                <MuiFileInput
-                                    {...field}
-                                    error={!!error}
-                                    helperText={error?.message as string}
-                                    inputProps={{accept: ".png, .jpeg, .jpg"}}
-                                />
-                            )}
-                            name="screenshots.1"
-                        />
-                        <Controller
-                            control={control}
-                            rules={{
-                                required: "入力してください",
-                                validate: (value) => {
-                                    if(value.type === "image/png" || value.type === "image/jpeg"){
-                                        return true
-                                    } else {
-                                        return "pngもしくはjpg画像を選択してください";
-                                    }
-                                }
-                            }}
-                            render={({field, fieldState: {error}}) => (
-                                    <MuiFileInput
-                                        {...field}
-                                        error={!!error}
-                                        helperText={error?.message as string}
-                                        inputProps={{accept: ".png, .jpeg, .jpg"}}
-                                    />
-                                )}
-                            name="screenshots.2"
-                        />
-                    </Stack>
-                    <Button type="submit" variant="contained">登録</Button>
-                </Stack>
+                        </Grid>
+                    </CardActions>
+                </Card>
 
             </Container>
         </>
