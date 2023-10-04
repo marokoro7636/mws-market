7b76a494efe6952f87b45a40b2f4238a77e8a8bf
stokob <entwicklung.st@gmail.com>
2023-10-04 19:53:47 +0900

    description生成のAPIの実装

--- api/helper/util.py
+++ api/helper/util.py
@@ -2,8 +2,13 @@ import hashlib
 import secrets
 import string
 from langchain.document_loaders import YoutubeLoader
-from langchain.chat_models import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.chains import SimpleSequentialChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def sha1_hash(data):
     return hashlib.sha1(data.encode('utf-8')).hexdigest()
@@ -13,10 +18,21 @@ def make_secret(length: int):
     secret = ''.join(secrets.choice(pass_chars) for x in range(length))
     return secret
 
-def make_description(url: str):
+def make_description(url: str, length):
+    chat = ChatOpenAI(temperature=0)
     loader = YoutubeLoader.from_youtube_url(url, language=["ja"])
     documents = loader.load()
-    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    summarize = load_summarize_chain(chat, chain_type="stuff", verbose=True)
-    result = summarize.run(documents)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+
+    prompt= PromptTemplate(
+    		input_variables=["text", "length"],
+    		template="""以下の文はツールの紹介文です。このツールを{length}文字で説明してください。:
+
+            "{text}"
+
+            """
+    )
+    chain = LLMChain(llm=chat, prompt=prompt)
+    result = chain.run(text = text_splitter.split_documents(documents), length=length)
+
     return result
--- api/routes/v0/project_option.py
+++ api/routes/v0/project_option.py
@@ -24,8 +24,23 @@ def get_project_description(project_id: str, x_auth_token: Optional[str] = Heade
     if youtube is None:
         raise StarletteHTTPException(status_code=404, detail="Youtube not found")
     try:
-        description = make_description(youtube)
+        description = make_description(youtube, 400)
     except:
-        raise StarletteHTTPException(
-            status_code=500, detail="Failed to get project description")
+        raise StarletteHTTPException(status_code=500, detail="Failed to get project description")
     return {"description": description}
+
+@router.get("/{project_id}/make_short_description")
+def get_project_short_description(project_id: str, x_auth_token: Optional[str] = Header(None)):
+    if not Project.is_exist(project_id):
+        raise StarletteHTTPException(status_code=404, detail="Project not found")
+    if not isAuthed(Teams(Project(project_id).get_team()).get_members(), x_auth_token):
+        raise StarletteHTTPException(status_code=401, detail="Unauthorized")
+
+    youtube = Project(project_id).get_youtube()
+    if youtube is None:
+        raise StarletteHTTPException(status_code=404, detail="Youtube not found")
+    try:
+        short_description = make_description(youtube, 100)
+    except:
+        raise StarletteHTTPException(status_code=500, detail="Failed to get project short description")
+    return {"short_description": short_description}
