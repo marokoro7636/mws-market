513d8aebcfb3096aba2db5520f58cf4a66ab92b5
Yushin-Yamamoto <y.yushin.yamamoto@gmail.com>
2023-10-05 14:51:20 +0900

    implement sanitize

--- api/helper/sanitize.py
+++ api/helper/sanitize.py
@@ -1,42 +1,32 @@
 import re
+import html
 
 
-def sanitizing_by_str(text: str):
-    # 記号をはじく
-    key = ["&", "<", ">", "'", '"', "/", "\\" ] # 要検証（"+", "*"）なども?
-    if text in key:
-        return False
-    else:
-        return True
+def sanitizing_by_html(text: str):
+    # 記号をエスケープ( "&", "<", ">", "'", '"' )
+    return html.escape(text)
 
-def sanitizing_by_len(text: str):
+def sanitizing_by_len(text: str, num: int):
     # 文字数制限
     # 半角英数字か判定（正規表現）
-    alnum = re.compile(r"^(\w)+$")
-    if alnum.match(text): ## or　if text.encode("utf-8").isalnum():
-        if len(text) < 30: # 調節想定
+    alnum = re.compile(r"^[a-zA-Z0-9]+$")
+    if alnum.match(text):
+        if len(text) <= num:
             return True
         else:
             return False
     else:
-        if len(text) < 60:
+        if len(text) <= 2*num:
             return True
         else:
             return False
         
 def sanitizing_by_id(id: str):
     # IDが sha1 であることの検証
-    sha1 = re.compile(r"^[a-f0-9]{40}$")
+    sha1 = re.compile(r"^[a-fA-F0-9]{40}$")
     if sha1.match(id):
         return True
     else:
         return False
-
-def sanitizing_by_file(filename: str):
-    # ファイル形式：jpg or ping + サイズ
-    # format = re.compile(r"jpeg$") 末尾に限定すべきか
-    if filename in ["jpeg", "png"]: # and (size < 500x500) 不明
-        return True
-    else:
-        return False
+    
     
