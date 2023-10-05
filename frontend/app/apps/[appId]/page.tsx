b439325e070a27d957b004a100d4cd1181431f1e
pfpfdev <pfpfdev@gmail.com>
2023-10-05 16:22:41 +0900

    implemented basic integration

--- frontend/app/apps/[appId]/page.tsx
+++ frontend/app/apps/[appId]/page.tsx
@@ -1,12 +1,13 @@
-import dynamic from 'next/dynamic'
-import React, { Suspense } from 'react'
-import {Box, Button, Container, Grid, Rating, Stack, Typography} from "@mui/material";
-import ScreenshotCarousel from "@/components/ScreenshotCarousel";
+'use client'
 
+import dynamic from 'next/dynamic'
+import React, { Suspense, useEffect, useState } from 'react'
+import { Box, Button, Container, Grid, Rating, Stack, Typography } from "@mui/material";
+import ScreenshotCarousel from "@/components/ScreenshotCarousel";
 
 const Editor = dynamic(() => import('@/components/MdEdtior'), { ssr: false })
 
-interface AppDetail{
+interface AppDetail {
     id: string,
     name: string,
     team: string,
@@ -27,68 +28,99 @@ interface AppDetail{
     }
 }
 
+type AppDetailData = {
+    id: string,
+    name: string,
+    team: string,
+    short_description: string,
+    description: string,
+    youtube: string,
+    details: {
+        img_screenshot: string[],
+        required_spec: {
+            item: string,
+            required: string
+        }[],
+        install: {
+            method: string,
+            info: string,
+            additional: string
+        }[],
+        forJob: string
+    },
+    review: {
+        // TODO
+    }
+    rating: {
+        // TODO
+    }
+    icon: string,
+    img: string
+}
+
 let markdown = `
 # Hello world!
 Check the EditorComponent.tsx file for the code .
 `
 
-const save = (md:string) => { markdown = md }
+const save = (md: string) => { markdown = md }
 
-// TODO 状態管理方法 ←Zustandが良さそう
 
 export default function Page({ params }: { params: { appId: string } }) {
     const appId = params.appId
-    // TODO /api/mock/projects/[appId]から情報を取ってくる
 
-    const appDetailMock: AppDetail = {
-        id: appId,
-        name: `App ${appId}`,
-        team: `team ${appId}`,
-        description: `description ${appId} `.repeat(50),
-        youtube: "https://youtu.be/ZaZMZ9jePKw?si=x96UEomj8VWoHw0-",
-        details: {
-            imgScreenshot: ["https://dummyimage.com/800x16:9/000/fff.png", "https://dummyimage.com/640x16:9/09f/fff.png", "https://dummyimage.com/400x16:9/cccccc/000000.png"],
-            requiredSpec: [
-                {
-                    item: "",
-                    required: ""
-                }
-            ],
-            install: [
-                {
-                    method: "",
-                    info: "",
-                    additional: ""
-                }
-            ],
-            forJob: ""
-        }
+    const [data, setData] = useState<AppDetailData | null>(null)
+
+    useEffect(() => {
+        fetch(`/api/v0/projects/${appId}`)
+            .then((response) => response.json())
+            .then((data) => setData(data))
+    }, [])
+
+    if (data === null) {
+        return <div>loading...</div>
     }
+
+    const appDetail: AppDetail = {
+        id: data.id,
+        name: data.name,
+        team: data.team,
+        description: data.short_description || "説明はまだ追加されていません",
+        youtube: data.youtube,
+        details: {
+            imgScreenshot: data.details.img_screenshot,
+            requiredSpec: data.details.required_spec,
+            install: data.details.install,
+            forJob: data.details.forJob
+        },
+    }
+
+    console.log(appDetail)
 
     return (
         <>
-            <Container sx={{mt: 3}}>
+            <Container sx={{ mt: 3 }}>
                 <Grid container alignItems="center">
                     <Grid item xs={3}>
-                        <img src="/icon128.png"/>
+                        <img src="/icon128.png" />
                     </Grid>
                     <Grid item xs={6}>
                         <Box>
-                            <Typography variant="h3">{appDetailMock.name}</Typography>
-                            <Typography variant="subtitle1">{appDetailMock.team}</Typography>
-                            <Rating name="read-only" value={3} size="small" sx={{mt: 2}}/>
+                            <Typography variant="h3">{appDetail.name}</Typography>
+                            <Typography variant="subtitle1">{appDetail.team}</Typography>
+                            <Rating name="read-only" value={3} size="small" sx={{ mt: 2 }} />
                         </Box>
                     </Grid>
                     <Grid item xs={3}>
-                        <Button variant="contained" sx={{width: 2/3, height: 50}}>インストール</Button>
+                        <Button variant="contained" sx={{ width: 2 / 3, height: 50 }}>インストール</Button>
                     </Grid>
                 </Grid>
                 <Stack spacing={2} mt={5}>
                     <Typography variant="h4">このアプリについて</Typography>
-                    <Typography component="div">{appDetailMock.description}</Typography>
+                    <Typography component="div">{appDetail.description}</Typography>
                 </Stack>
-                <Box sx={{mt: 5}}>
-                    <ScreenshotCarousel imgList={appDetailMock.details.imgScreenshot}/>
+                <Box sx={{ mt: 5 }}>
+                    <ScreenshotCarousel imgList={appDetail.details.imgScreenshot} />
                 </Box>
                 <Stack spacing={2} mt={5}>
                     <Typography variant="h4">紹介動画</Typography>
--- frontend/components/ScreenshotCarousel.tsx
+++ frontend/components/ScreenshotCarousel.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import AliceCarousel from "react-alice-carousel";
 import 'react-alice-carousel/lib/alice-carousel.css';
 
@@ -8,9 +8,24 @@ interface ScreenshotCarouselProps {
     imgList: string[]
 }
 
-const ScreenshotCarousel = ({imgList}: ScreenshotCarouselProps) => {
+const ScreenshotCarousel = ({ imgList }: ScreenshotCarouselProps) => {
     const imgWidth = 500 // スクリーンショットの横幅
     const spacing = 20 // スクリーンショット同士の間
+
+    if (!Array.isArray(imgList) || imgList.length === 0) {
+        const dummyPlaceholder = "https://placehold.jp/45/b3bac1/ffffff/640x360.jpg?text=Image%20is%20not%20available"
+        const items = [
+            <Box width={imgWidth + spacing}>
+                <Box component="img" src={dummyPlaceholder} draggable={false} width={imgWidth} />
+            </Box>
+        ]
+        return (
+            <AliceCarousel
+                mouseTracking
+                autoWidth
+                items={items}
+            />)
+    }
 
     return (
         <AliceCarousel
@@ -19,8 +34,8 @@ const ScreenshotCarousel = ({imgList}: ScreenshotCarouselProps) => {
             infinite
             items={
                 imgList.map((item, i) => (
-                    <Box width={imgWidth+spacing} key={i}>
-                        <Box component="img" src={item} draggable={false} width={imgWidth}/>
+                    <Box width={imgWidth + spacing} key={i}>
+                        <Box component="img" src={item} draggable={false} width={imgWidth} />
                     </Box>
                 ))
             }
