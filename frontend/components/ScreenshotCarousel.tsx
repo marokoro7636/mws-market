317c7338f6b92670b419224b571933f952d3c43d
pfpfdev <pfpfdev@gmail.com>
2023-10-05 17:19:38 +0900

    implemented basic integration

--- frontend/components/ScreenshotCarousel.tsx
+++ frontend/components/ScreenshotCarousel.tsx
@@ -1,16 +1,19 @@
 "use client"
 import React from 'react';
-import { Box } from "@mui/material";
+import {Box, IconButton} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 import AliceCarousel from "react-alice-carousel";
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 interface ScreenshotCarouselProps {
-    imgList: string[]
+    imgList: string[],
+    editable: boolean,
+    onDelete: (url: string) => void
 }
 
-const ScreenshotCarousel = ({ imgList }: ScreenshotCarouselProps) => {
+const ScreenshotCarousel = ({imgList, editable, onDelete}: ScreenshotCarouselProps) => {
     const imgWidth = 500 // スクリーンショットの横幅
-    const spacing = 20 // スクリーンショット同士の間
+    const spacing = 30 // スクリーンショット同士の間
 
     if (!Array.isArray(imgList) || imgList.length === 0) {
         const dummyPlaceholder = "https://placehold.jp/45/b3bac1/ffffff/640x360.jpg?text=Image%20is%20not%20available"
@@ -31,11 +34,16 @@ const ScreenshotCarousel = ({ imgList }: ScreenshotCarouselProps) => {
         <AliceCarousel
             mouseTracking
             autoWidth
-            infinite
+            disableButtonsControls
             items={
                 imgList.map((item, i) => (
-                    <Box width={imgWidth + spacing} key={i}>
-                        <Box component="img" src={item} draggable={false} width={imgWidth} />
+                    <Box width={imgWidth+spacing} key={i} sx={{position: "relative"}}>
+                        <Box component="img" src={item} draggable={false} width={imgWidth}/>
+                        {editable &&
+                            <IconButton color="error" onClick={() => {onDelete(item)}} sx={{position: "absolute", top: 10, right: spacing+10}}>
+                                <DeleteIcon />
+                            </IconButton>
+                        }
                     </Box>
                 ))
             }
