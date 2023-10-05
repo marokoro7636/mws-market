ca34f1cace70cbdaececd41eec8a42c83c9745f1
pfpfdev <pfpfdev@gmail.com>
2023-10-06 00:51:57 +0900

    deleted duplicate swagger samples

--- api/routes/v0_router.py
+++ api/routes/v0_router.py
@@ -5,7 +5,7 @@ from routes.v0.user import router as users_router
 
 def get_router():
     r = APIRouter()
-    r.include_router(projects_router, prefix="/projects", tags=["v0_projects"])
+    r.include_router(projects_router, prefix="/projects")
     r.include_router(teams_router, prefix="/teams", tags=["v0_teams"])
     r.include_router(users_router, prefix="/users", tags=["v0_users"])
     return r
