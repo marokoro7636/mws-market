fc8c292d5a8b1c10776e85cf69818da8957a1eb3
pfpfdev <pfpfdev@gmail.com>
2023-10-06 14:42:57 +0900

    improved ui

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -3,12 +3,15 @@
 import React from "react"
 import { signIn, signOut, useSession } from "next-auth/react"
 import Button from '@mui/material/Button';
+import { CircularProgress } from '@mui/material';
 
 export default function Home() {
     const { data: session, status } = useSession()
 
     if (status === "loading") {
-        return <p>Loading...</p>
+        return <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <CircularProgress />
+        </div>
     }
     console.log(session)
 
