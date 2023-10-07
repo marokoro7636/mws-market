e22059388ceb6e35478357d34b1400681000c36c
stokob <entwicklung.st@gmail.com>
2023-10-08 01:41:38 +0900

    youtubeとgithubを用いたdescriptionの自動生成

--- api/helper/util.py
+++ api/helper/util.py
@@ -9,6 +9,13 @@ from langchain.chains import SimpleSequentialChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+import re
+import json
+import base64
+from  urllib.parse import unquote, urlparse
+import urllib.request
+from typing import Optional
 
 def sha1_hash(data):
     return hashlib.sha1(data.encode('utf-8')).hexdigest()
@@ -18,21 +25,72 @@ def make_secret(length: int):
     secret = ''.join(secrets.choice(pass_chars) for x in range(length))
     return secret
 
-def make_description(url: str, length):
+def make_description(youtube: Optional[str] = None, github: Optional[str] = None, description: Optional[str] = None):
+    youtube = get_script(youtube)
+    github = get_readme(github)
+    description = url_decode(description)
+
     chat = ChatOpenAI(temperature=0)
-    loader = YoutubeLoader.from_youtube_url(url, language=["ja"])
-    documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
 
     prompt= PromptTemplate(
-    		input_variables=["text", "length"],
-    		template="""以下の文はツールの紹介文です。このツールを{length}文字で説明してください。:
+    		input_variables=["text"],
+    		template="""以下の文はあるツールの紹介文の要約です。このツールを日本語で400文字で説明してください。:
 
             "{text}"
 
             """
     )
-    chain = LLMChain(llm=chat, prompt=prompt)
-    result = chain.run(text = text_splitter.split_documents(documents), length=length)
 
-    return result
+    documents = [youtube, github, description]
+    source = {medium: "ok" if document is not None  else "ng" for medium, document in zip(["youtube", "github", "description"], documents)}
+    documents = [x for x in documents if x is not None]
+
+    summarize = load_summarize_chain(llm=chat, chain_type="map_reduce", verbose=False)
+    text = summarize.run(text_splitter.split_documents(documents))
+
+    chain = LLMChain(llm=chat, prompt=prompt)
+
+    result = chain.run(text = text)
+    return result, source
+
+def get_script(url: str):
+    if url is None or re.match(r'^https?:\/\/(www.youtube.com\/|youtu.be\/)(\w+)', url) is None:
+        return None
+    parse = urlparse(url)
+    if re.match(r'^https?:\/\/www.youtube.com\/(\w+)', url) is not None:
+        id = urllib.parse.parse_qs(parse.query).get("v")[0]
+    if re.match(r'^https?:\/\/youtu.be\/(\w+)', url) is not None:
+        id = parse.path.split('/')[1]
+
+    url = f"https://youtu.be/{id}"
+    try:
+        loader = YoutubeLoader.from_youtube_url(url, language=["ja"])
+        documents = loader.load()
+    except:
+        return None
+    return documents[0]
+
+def get_readme(url: str):
+    if url is None or re.match(r'https:\/\/github.com\/(\w+)', url) is None:
+        return None
+    parse = urlparse(url).path.split('/')
+    owner = parse[1]
+    repos = parse[2]
+    url = f"https://api.github.com/repos/{owner}/{repos}/readme"
+    headers = {"Accept": "application/vnd.github+json"}
+    request = urllib.request.Request(url, headers=headers)
+    try:
+        with urllib.request.urlopen(request) as res:
+            response = json.load(res)
+    except urllib.error.HTTPError:
+        return None
+
+    text = base64.b64decode(response.get("content")).decode()
+    return Document(page_content=text[:min(len(text) // 5, 1000)])
+
+def url_decode(description: str):
+    if description is None:
+        return None
+    else:
+        return Document(page_content=unquote(description))
--- api/routes/v0/project_option.py
+++ api/routes/v0/project_option.py
@@ -2,7 +2,13 @@ from fastapi import APIRouter
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from typing import Optional
 from fastapi import Header
-from helper.util import make_description
+from helper.util import(
+    get_script,
+    get_readme,
+    is_youtube,
+    is_github,
+    make_description
+)
 from fastapi import UploadFile
 import datetime
 from helper.auth import isAuthed
@@ -13,36 +19,11 @@ from models.teams import Teams
 
 router = APIRouter()
 
-@router.get("/{project_id}/make_description")
-def get_project_description(project_id: str, x_auth_token: Optional[str] = Header(None)):
-    if not Project.is_exist(project_id):
-        raise StarletteHTTPException(status_code=404, detail="Project not found")
-    if not isAuthed(Teams(Project(project_id).get_team()).get_members(), x_auth_token):
-        raise StarletteHTTPException(status_code=401, detail="Unauthorized")
-
-    youtube = Project(project_id).get_youtube()
-    if youtube is None:
-        raise StarletteHTTPException(status_code=404, detail="Youtube not found")
+@router.get("/make_description")
+def get_description(youtube: Optional[str] = None, github: Optional[str] = None, description: Optional[str] = None):
     try:
-        description = make_description(youtube, 400)
+        description, source = make_description(youtube, github, description)
     except Exception as e:
         print(e)
         raise StarletteHTTPException(status_code=500, detail="Failed to get project description")
-    return {"description": description}
-
-@router.get("/{project_id}/make_short_description")
-def get_project_short_description(project_id: str, x_auth_token: Optional[str] = Header(None)):
-    if not Project.is_exist(project_id):
-        raise StarletteHTTPException(status_code=404, detail="Project not found")
-    if not isAuthed(Teams(Project(project_id).get_team()).get_members(), x_auth_token):
-        raise StarletteHTTPException(status_code=401, detail="Unauthorized")
-
-    youtube = Project(project_id).get_youtube()
-    if youtube is None:
-        raise StarletteHTTPException(status_code=404, detail="Youtube not found")
-    try:
-        short_description = make_description(youtube, 100)
-    except Exception as e:
-        print(e)
-        raise StarletteHTTPException(status_code=500, detail="Failed to get project short description")
-    return {"short_description": short_description}
+    return {"description": description, "src": source}
